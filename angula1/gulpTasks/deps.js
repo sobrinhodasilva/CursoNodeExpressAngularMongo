08bd2e0da0e5694f4f06499a17c0531b90e53bb0
Roberto Sobrinho da Silva <sobrinhodasilva@gmail.com>
2017-10-01 23:06:22 -0300

    Estrutura inicial do bild (iniciando aquivos gulp )

---
+++ angula1/gulpTasks/deps.js
@@ -0,0 +1,15 @@
+const gulp = require('gulp')
+
+gulp.task('deps', ['deps.js', 'deps.css', 'deps.fonts'])
+
+gulp.task('deps.js', () => {
+
+})
+
+gulp.task('deps.css', () => {
+
+})
+
+gulp.task('deps.fonts', () => {
+  
+})
