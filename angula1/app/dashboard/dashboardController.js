cabaf1bc8902f200abce9e153663365c743207f0
Roberto Sobrinho da Silva <sobrinhodasilva@gmail.com>
2017-10-04 21:01:32 -0300

    dashboard controler

---
+++ angula1/app/dashboard/dashboardController.js
@@ -0,0 +1,8 @@
+angular.module('primeiraApp').controller('DashboardCtrl',[
+  '$scope',
+  '$http',
+  DashboardController
+])
+function DashboardController($scope, $http){
+  $scope.getSummary = function (){}
+}
--- backend/config/routes.js
+++ backend/config/routes.js
@@ -10,4 +10,7 @@ module.exports = function(server){
   const billingCycleService = require('../api/billingCycle/billingCycleService')
   // Registra a Url raiz billingCycles
   billingCycleService.register(router, '/billingCycles')
+
+  const billingSummaryService = require('../api/billingSummary/billingSummaryService')
+  router.route('/billingSummary').get(billingSummaryService.getSummary)
 }
