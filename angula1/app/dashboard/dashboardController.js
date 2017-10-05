5ef676dcf710f7f6331454008ac4e5be27aef503
Roberto Sobrinho da Silva <sobrinhodasilva@gmail.com>
2017-10-05 00:02:59 -0300

    correçao variavel vm

--- angula1/app/dashboard/dashboardController.js
+++ angula1/app/dashboard/dashboardController.js
@@ -1,5 +1,5 @@
 // refactory IIFE Immediately Invoked Function Expression
-// styleguide Johnpapa 
+// styleguide Johnpapa
 // Removendo variáveis do escopo global
 (function () {
 
@@ -10,17 +10,17 @@
   function DashboardController( $http){
     const vm = this  // Padrão ViewModel styleguide
     //definir a funcao getSummary no scope
-    $vm.getSummary = function (){
+    vm.getSummary = function (){
       const url = 'http://localhost:3003/api/billingSummary'
       $http.get(url).then(function(response){
         const {credit = 0, debt = 0} = response.data
-        $vm.credit = credit
-        $vm.debt = debt
-        $vm.total = credit - debt
+        vm.credit = credit
+        vm.debt = debt
+        vm.total = credit - debt
       })
     }
     //Chamar a funcao para ser executada
-    $vm.getSummary()
+    vm.getSummary()
   }
 
 })() //IIFE
