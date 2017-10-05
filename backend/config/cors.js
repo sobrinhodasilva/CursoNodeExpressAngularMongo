63f1664b0a7eefdbf7070eac29000757efed97dc
Roberto Sobrinho da Silva <sobrinhodasilva@gmail.com>
2017-10-04 22:45:42 -0300

    Corrigindo variavel res.

--- backend/config/cors.js
+++ backend/config/cors.js
@@ -1,9 +1,9 @@
-// middleware QUE LIBERA Acesso a requisiçoes de outros servidores
+// middleware QUE LIBERA Acesso a requisiçoes CORs de outros servidores
 
 module.exports = function(req, res, next){
-  rest.header('Access-Control-Allow-Origin', '*')
-  rest.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT , PATCH, DELETE'),
-  rest.header('Access-Control-Allow-Headers', 'Origin, X-Request-With, Content-Type, Accept')
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT , PATCH, DELETE'),
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Request-With, Content-Type, Accept')
   //Vida que se segue
   next()
 }
