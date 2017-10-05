1db5c199b5121794e5d9b753017c6f1f4dddc27b
Roberto Sobrinho da Silva <sobrinhodasilva@gmail.com>
2017-10-05 03:12:35 -0300

    controller metodo refresh

--- angula1/app/billingCycle/billingCycleController.js
+++ angula1/app/billingCycle/billingCycleController.js
@@ -8,12 +8,22 @@
 
   function BillingCycleController($http, msgs){
     const vm = this
+    const url = 'http://localhost:3003/api/billingCycles'
+
+    //Atualiza após uma oprecão q altere os dados 
+    vm.refresh = function () {
+      $http.get(url, vm.billingCycle).then(
+        function(response){
+          vm.billingCycle = {}
+          vm.billingCycles = response.data
+        }
+      )
+    }
 
     vm.create = function (){
-        const url = 'http://localhost:3003/api/billingCycles'
         $http.post(url, vm.billingCycle).then(
           function (response){
-            vm.billingCycle ={}
+            vm.refresh()
             //console.log('POst Sucesso! ')
             msgs.addSuccess('Operação realizada com Sucesso!!')
           },
