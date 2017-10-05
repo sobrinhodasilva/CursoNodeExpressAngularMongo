7917fb637e2161ff81c0756983c4c403bcf2e161
Roberto Sobrinho da Silva <sobrinhodasilva@gmail.com>
2017-10-04 23:38:55 -0300

    boas praticas refactory IIFE, controlle as e ViewModel

--- angula1/app/dashboard/dashboardController.js
+++ angula1/app/dashboard/dashboardController.js
@@ -1,19 +1,26 @@
-angular.module('primeiraApp').controller('DashboardCtrl',[
-  '$scope',
-  '$http',
-  DashboardController
-])
-function DashboardController($scope, $http){
-  //definir a funcao getSummary no scope
-  $scope.getSummary = function (){
-    const url = 'http://localhost:3003/api/billingSummary'
-    $http.get(url).then(function(response){
-      const {credit = 0, debt = 0} = response.data
-      $scope.credit = credit
-      $scope.debt = debt
-      $scope.total = credit - debt
-    })
+// refactory IIFE Immediately Invoked Function Expression
+// styleguide Johnpapa 
+// Removendo variáveis do escopo global
+(function () {
+
+  angular.module('primeiraApp').controller('DashboardCtrl',[
+    '$http',
+    DashboardController
+  ])
+  function DashboardController( $http){
+    const vm = this  // Padrão ViewModel styleguide
+    //definir a funcao getSummary no scope
+    $vm.getSummary = function (){
+      const url = 'http://localhost:3003/api/billingSummary'
+      $http.get(url).then(function(response){
+        const {credit = 0, debt = 0} = response.data
+        $vm.credit = credit
+        $vm.debt = debt
+        $vm.total = credit - debt
+      })
+    }
+    //Chamar a funcao para ser executada
+    $vm.getSummary()
   }
-  //Chamar a funcao para ser executada
-  $scope.getSummary()
-}
+
+})() //IIFE
