43ec9370a9166d9d428d5bbe3c88b9c2399af676
Roberto Sobrinho da Silva <sobrinhodasilva@gmail.com>
2017-10-06 00:12:45 -0300

    Calcular o sumário

--- angula1/app/billingCycle/billingCycleController.js
+++ angula1/app/billingCycle/billingCycleController.js
@@ -17,7 +17,7 @@
         function(response){
           vm.billingCycle = {credits:[{}], debts:[{}]}
           vm.billingCycles = response.data
-
+          vm.calculateValues()
           //mostrar as abas necessarias visiveis
           tabs.show(vm, {tabList:true, tabCreate:true})
         }
@@ -40,6 +40,7 @@
     //Update
     vm.showTabUpdate = function(billingCycle){
       vm.billingCycle = billingCycle
+      vm.calculateValues()
       tabs.show(vm, {tabUpdate:true})
     }
     vm.update = function () {
@@ -60,6 +61,7 @@
     //Delete
     vm.showTabDelete = function(billingCycle){
       vm.billingCycle = billingCycle
+      vm.calculateValues()
       tabs.show(vm, {tabDelete:true})
     }
     vm.delete = function () {
@@ -84,10 +86,12 @@
     }
     vm.cloneCredit = function (index, {name, value}) {
       vm.billingCycle.credits.splice(index + 1, 0, {name, value})
+      vm.calculateValues()
     }
     vm.deleteCredit = function (index) {
       if (vm.billingCycle.credits.length > 1) {
         vm.billingCycle.credits.splice(index, 1)
+        vm.calculateValues()
       }
     }
     //Debitos
@@ -96,13 +100,31 @@
     }
     vm.cloneDebt = function (index, {name, value, status}) {
       vm.billingCycle.debts.splice(index + 1, 0, {name, value, status})
+      vm.calculateValues()
     }
     vm.deleteDebt = function (index) {
       if (vm.billingCycle.debts.length > 1) {
         vm.billingCycle.debts.splice(index, 1)
+        vm.calculateValues()
       }
     }
-    
+
+    //Sumário Credito e debitos
+    vm.calculateValues = function () {
+      vm.credit = 0
+      vm.debt = 0
+
+      if (vm.billingCycle) {
+        vm.billingCycle.credits.forEach(function ({value}) {
+          vm.credit += !value || isNaN(value)? 0 : parseFloat(value)
+        })
+        vm.billingCycle.debts.forEach(function ({value}) {
+          vm.debt += !value || isNaN(value)? 0 : parseFloat(value)
+        })
+      }
+      vm.total = vm.credit - vm.debt
+    }
+
     //Chamar a funçao refresh sempre q acessar o controller
     vm.refresh()
   }
